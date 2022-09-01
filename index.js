f4702f80219fe1d4c4dd9b5caf10937d2da3c96a
chrischep <christine.chepleting@student.moringaschool.com>
2022-09-01 14:31:33 +0300

    render one car

---
+++ index.js
@@ -0,0 +1,30 @@
+//event listener
+// document.querySelector('car-form').addEventListener('submit',
+// handleSubmit)
+//Dom render functions
+function renderOneCar(car){
+    //build car
+    let card=document.createElement('li')
+    card.className='card'
+    card.innerHTML=`
+    <img src ="${car.imageURL}">
+    <div class="content">
+    <h4>${car.name}</h4>
+    <p>
+    $<span class="like-count">
+    ${car.likes}</span>Liked
+    </p>
+    <p>${car.description}</p>
+    </div>
+    <div id="buttons">
+    <button id="like">Like</button>
+    <button id="delete">Delete car</button>
+    </div>
+    `
+
+}
+function initialize(){
+  carData.forEach (car=>
+    renderOneCar(car)) 
+}
+initialize()
